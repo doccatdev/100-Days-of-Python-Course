f8fe0f6933ffc7c92a7b5b0772d54cd810fcc65d
Muhamad Yusuf Hidayat <40491907+readdoc-png@users.noreply.github.com>
2023-10-12 10:26:11 +0700

    Update 1.6.py

--- Exercise/1.6.py
+++ Exercise/1.6.py
@@ -96,3 +96,9 @@ output = "The tax is Rp.%.2f and the tip is Rp.%.2f, making the total Rp.%.2f" %
 
 #Display output
 print(output)
+
+#Exercise 7 : Sum of the first n Positive Integers
+number = input("Give a number :")
+int_number = int(number)
+sum = (int_number) * (int_number + 1) / 2
+print("The sum of the first", int_number, "positive integer is", sum)
