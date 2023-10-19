fad659824848083c7aa8c0957d3ba5d0e1eeec04
Muhamad Yusuf Hidayat <40491907+readdoc-png@users.noreply.github.com>
2023-10-19 09:03:31 +0700

    Update 1.6 Exercise.py

--- Exercise/1.6 Exercise.py
+++ Exercise/1.6 Exercise.py
@@ -159,3 +159,60 @@ print(x, "/", y, "is", x / y)
 print("The base 10 logarithm of", x, "is", log10(x))
 print(x, "x^y", y, "is", x**y)
 
+# Exercise 11 : Fuel Efficiency
+print("Fuel Efficiency")
+x = input("Type a number : ")
+x = float(x)
+calculate = 235 / x
+print(calculate, "L/100KM")
+
+# Exercise 12 : Distance Between Two Points on Earth
+
+
+import math
+
+print("Distance Between Two Points on Earth")
+
+# # Latitude
+t1 = float(input("Give input for t1 : "))
+t1 = math.radians(t1)
+
+g1 = float(input("Give input for g1 : "))
+g1 = math.radians(g1)
+
+# # Longitude
+t2 = float(input("Give input for t2 : "))
+t2 = math.radians(t2)
+
+g2 = float(input("Give input for g2 : "))
+g2 = math.radians(g2)
+
+# #Dilatation
+dlat = t2 - t1
+dlon = g2 - g1
+
+# formula to calculate distance between two point of earth
+a = math.sin(dlat / 2) ** 2 + math.cos(t1) * math.cos(t2) * math.sin(dlon / 2) ** 2
+c = 2 * math.atan2(math.sqrt(a), math.sqrt(1 - a))
+
+distance = 6371.01 * c
+print("The distance between the two points is {:.2f} kilometers.".format(distance))
+
+
+# Exercise 13 : Making Change
+
+PENNY_COINS = 1
+NICKEL_COINS = 5
+DIME_COINS = 10
+QUARTER_COINS = 25
+LOONIE_COINS = 100
+TONIE_COINS = 200
+
+change = input("Masukan kembalian yang ingin dipecahkan $")
+change = int(change)
+
+print(" ", change // TONIE_COINS, "toonies")
+change = change % TONIE_COINS
+
+
+# Exercise 14 : Height Uniy
