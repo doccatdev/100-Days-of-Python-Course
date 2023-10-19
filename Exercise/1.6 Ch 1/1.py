b308d8c734e7465b4acc930a49f7291635a08460
Muhamad Yusuf Hidayat <40491907+readdoc-png@users.noreply.github.com>
2023-10-19 10:22:11 +0700

    Delete Exercise directory

--- Exercise/1.6 Ch 1/1.py
+++
@@ -1,9 +0,0 @@
-#Exercise 1 : Mailing Address
-
-print("Muhamad Yusuf Hidayat")
-print("Departement of Computer Science")
-print("National Institute Technology Bandung")
-print("2500 National Institute Technology Bandung")
-print("4XXXX, Bandung")
-print("Indonesia")
-
