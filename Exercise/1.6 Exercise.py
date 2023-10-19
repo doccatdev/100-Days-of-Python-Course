52cb234e32d8c84c242333de10001a79da7ca6fe
Muhamad Yusuf Hidayat <40491907+readdoc-png@users.noreply.github.com>
2023-10-19 09:33:07 +0700

    Update 1.6 Exercise.py

--- Exercise/1.6 Exercise.py
+++ Exercise/1.6 Exercise.py
@@ -215,4 +215,19 @@ print(" ", change // TONIE_COINS, "toonies")
 change = change % TONIE_COINS
 
 
-# Exercise 14 : Height Uniy
+# Exercise 14 : Height Unit
+
+IN_PER_FT = 12
+CM_PER_IN = 2.54
+
+
+# Read the height from the user
+print("Enter your height:")
+feet = int(input(" Number of feet: "))
+inches = int(input(" Number of inches: "))
+
+# Compute the equivalent number of centimeters
+cm = (feet * IN_PER_FT + inches) * CM_PER_IN
+
+# Display the result
+print("Your height in centimeters is:", cm)
