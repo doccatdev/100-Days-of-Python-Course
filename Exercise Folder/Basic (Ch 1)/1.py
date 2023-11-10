42e00445a90fd81c8ec45c8462228825e7e26355
Muhamad Yusuf Hidayat <40491907+readdoc-png@users.noreply.github.com>
2023-11-10 19:30:50 +0700

    Create 1.py

---
+++ Exercise Folder/Basic (Ch 1)/1.py
@@ -0,0 +1,9 @@
+#Exercise 1 : Mailing Address
+
+print("Muhamad Yusuf Hidayat")
+print("Departement of Computer Science")
+print("National Institute Technology Bandung")
+print("2500 National Institute Technology Bandung")
+print("4XXXX, Bandung")
+print("Indonesia")
+
