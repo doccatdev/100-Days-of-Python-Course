0cb69004d84fb49e4ce9f7a08ff943d539738ab2
Muhamad Yusuf Hidayat <40491907+doccatdev@users.noreply.github.com>
2025-02-05 23:05:19 +0700

    Delete Day 7 directory

--- Day 7/Hangman Game/hangman_game.py
+++
@@ -1,7 +0,0 @@
-word_list = [
-    "rumah", "sekolah", "jalanan", "pohon", "air", "langit", "hujan", "buku", "meja", "kursi",
-    "motor", "mobil", "jalanan", "tas", "bunga", "lampu", "gelas", "piring", "sendok", "garpu",
-    "televisi", "radio", "komputer", "laptop", "handphone", "kucing", "anjing", "burung", "ikan", "ular",
-    "pintu", "jendela", "tembok", "atap", "lantai", "kapal", "pesawat", "gunung", "sungai", "pantai",
-    "bola", "raket", "sepatu", "kaos", "celana", "jaket", "jam", "kertas", "pensil", "penghapus"
-]
