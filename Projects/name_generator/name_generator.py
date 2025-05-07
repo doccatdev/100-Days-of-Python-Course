b7376d418e67a1f38982c6b0f36603b660cdeb54
Muhamad Yusuf Hidayat <40491907+doccatdev@users.noreply.github.com>
2025-05-07 19:21:25 +0700

    Add files via upload

---
+++ Projects/name_generator/name_generator.py
@@ -0,0 +1,57 @@
+import random
+
+
+
+print("Welcome to the Psych 'Sidekick Name Picker.'\n")
+print("A name just like Sean would pick for Gus:\n\n")
+
+first_name = ['Baby Oil', 'Bad News', 'Big Burps', "Bill 'Beenie-Weenie'",
+         "Bob 'Stinkbug'", 'Bowel Noises', 'Boxelder', "Bud 'Lite' ",
+         'Butterbean', 'Buttermilk', 'Buttocks', 'Chad', 'Chesterfield',
+         'Chewy', 'Chigger", "Cinnabuns', 'Cleet', 'Cornbread', 'Crab Meat',
+         'Crapps', 'Dark Skies', 'Dennis Clawhammer', 'Dicman', 'Elphonso',
+         'Fancypants', 'Figgs', 'Foncy', 'Gootsy', 'Greasy Jim', 'Huckleberry',
+         'Huggy', 'Ignatious', 'Jimbo', "Joe 'Pottin Soil'", 'Johnny',
+         'Lemongrass', 'Lil Debil', 'Longbranch', '"Lunch Money"', 
+         'Mergatroid', '"Mr Peabody"', 'Oil-Can', 'Oinks', 'Old Scratch', 
+         'Ovaltine', 'Pennywhistle', 'Pitchfork Ben', 'Potato Bug', 
+         'Pushmeet','Rock Candy', 'Schlomo', 'Scratchensniff', 'Scut',
+         "Sid 'The Squirts'", 'Skidmark', 'Slaps', 'Snakes', 'Snoobs',
+         'Snorki', 'Soupcan Sam', 'Spitzitout', 'Squids', 'Stinky',
+         'Storyboard', 'Sweet Tea', 'TeeTee', 'Wheezy Joe',
+         "Winston 'Jazz Hands'", 'Worms']
+
+last_name = ['Appleyard', 'Bigmeat', 'Bloominshine', 'Boogerbottom',
+        'Breedslovetrout', 'Butterbaugh', 'Clovenhoof', 'Clutterbuck',
+        'Cocktoasten', 'Endicott', 'Fewhairs', 'Gooberdapple', 'Goodensmith',
+        'Goodpasture', 'Guster', 'Henderson', 'Hooperbag', 'Hoosenater',
+        'Hootkins', 'Jefferson', 'Jenkins', 'Jingley-Schmidt', 'Johnson',
+        'Kingfish', 'Listenbee', "M'Bembo", 'McFadden', 'Moonshine', 'Nettles',
+        'Noseworthy', 'Olivetti', 'Outerbridge', 'Overpeck', 'Overturf',
+        'Oxhandler', 'Pealike', 'Pennywhistle', 'Peterson', 'Pieplow',
+        'Pinkerton', 'Porkins', 'Putney', 'Quakenbush', 'Rainwater',
+        'Rosenthal', 'Rubbins', 'Sackrider', 'Snuggleshine', 'Splern',
+        'Stevens', 'Stroganoff', 'Sugar-Gold', 'Swackhamer', 'Tippins',
+        'Turnipseed', 'Vinaigrette', 'Walkingstick', 'Wallbanger', 'Weewax',
+        'Weiners', 'Whipkey', 'Wigglesworth', 'Wimplesnatch', 'Winterkorn',
+        'Woolysocks']
+
+while True:
+
+    first_name_random = random.choice(first_name)
+    #print(first_name_random)
+
+    last_name_random = random.choice(last_name)
+    #print(last_name_random)
+
+    full_name = first_name_random +" " + last_name_random
+    print(full_name)
+
+    restart = input("Try Again? (Press Y to Restart Game or Enter to quit)")
+    if restart.lower() == "Y":
+        continue
+    else:
+        break
+
+
+    
